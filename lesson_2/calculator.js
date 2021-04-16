cfc0ea098e838c15e8fcd1208818c3ffce5d9ec9
Ghislain Malardier <ghislain.malardier@gmail.com>
2021-04-16 11:02:10 -0700

    Calculator Bonus Features

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,4 +1,6 @@
-const userInput = require('readline-sync');
+const USER_INPUT = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
+const LANGUAGE = 'fr';
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -7,43 +9,57 @@ function invalidNumber(num) {
   return num.trimStart() === '' || Number.isNaN(Number(num));
 }
 
+function  calculator(message, lang = 'en') {
+  return MESSAGES[lang][message];
+}
+
+let newCalculation = 'y';
+
 // Welcome user
-prompt('Welcome to the calculator!');
-// Ask for a number
-let number1 = userInput.question(prompt('Enter a first number: '));
-while (invalidNumber(number1)) {
-  number1 = userInput.question(prompt('Hummm... that doesn\'t look like a valid number!\n Please enter a number: '));
-}
-// Ask for another number
-let number2 = userInput.question(prompt('Enter a second number: '));
-while (invalidNumber(number2)) {
-  number2 = userInput.question(prompt('Hummm... that doesn\'t look like a valid number!\n Please re-enter the second number: '));
-}
-// Ask for the type of caculation
-let operator = userInput.question(prompt('What operation would you like to do?\n1) Add 2) Subtract 3) Multiply 4) Divide'));
-while (!['1', '2', '3', '4'].includes(operator)) {
-  operator = userInput.question(prompt('You input isn\'t valid! Please choose one of the following:\n1) Add 2) Subtract 3) Multiply 4) Divide'));
+prompt(calculator('welcome', LANGUAGE));
+
+// Loop
+while (newCalculation === 'y') {
+
+  // Ask for a number
+  let number1 = USER_INPUT.question(prompt(calculator('firstNumber', LANGUAGE)));
+  while (invalidNumber(number1)) {
+    number1 = USER_INPUT.question(prompt(calculator('firstNumberInvalid', LANGUAGE)));
+  }
+  // Ask for another number
+  let number2 = USER_INPUT.question(prompt(calculator('secondNumber', LANGUAGE)));
+  while (invalidNumber(number2)) {
+    number2 = USER_INPUT.question(prompt(calculator('secondNumberinvalid', LANGUAGE)));
+  }
+  // Ask for the type of caculation
+  let operator = USER_INPUT.question(prompt(calculator('operatorQuestion', LANGUAGE)));
+  while (!['1', '2', '3', '4'].includes(operator)) {
+    operator = USER_INPUT.question(prompt(calculator('operatorQuestionInvalid', LANGUAGE)));
+  }
+
+  // Do the math
+  let result;
+
+  switch (operator) {
+    case '1':
+      result = Number(number1) + Number(number2);
+      break;
+    case '2':
+      result = Number(number1) - Number(number2);
+      break;
+    case '3':
+      result = Number(number1) * Number(number2);
+      break;
+    case '4':
+      result = Number(number1) / Number(number2);
+      break;
+  }
+
+  // Display the result
+  console.log(calculator('resultStart') + result + calculator('resultEnd'));
+  newCalculation = USER_INPUT.question(prompt(calculator('newCalculationQuestion', LANGUAGE)));
+  while (!['y', 'n'].includes(newCalculation)) {
+    newCalculation = USER_INPUT.question(prompt(calculator('newCalculationQuestionInvalid', LANGUAGE)));
+  }
 }
 
-// Do the math
-let result;
-
-switch (operator) {
-  case '1':
-    result = Number(number1) + Number(number2);
-    break;
-  case '2':
-    result = Number(number1) - Number(number2);
-    break;
-  case '3':
-    result = Number(number1) * Number(number2);
-    break;
-  case '4':
-    result = Number(number1) / Number(number2);
-    break;
-  default:
-    console.log('The information you input is incorrect. Please try again.');
-}
-
-// Display the result
-console.log(`The result is ${result}.`);
