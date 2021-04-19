d0810822bea67c391a2b97183af7b797cf53ed0f
Ghislain Malardier <ghislain.malardier@gmail.com>
2021-04-19 14:25:37 -0700

    Add rock, paper, scisssor game

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -15,7 +15,7 @@ const SPOCK = 'spock';
 const CHOICES = [ROCK, PAPER, SCISSORS, LIZARD, SPOCK];
 const CHOICES_DEFINITION = ['r = rock', 'p = paper', 'sc = scissors', 'l = lizard', 'sp = spock'];
 
-function prompt(msg, number) {
+function prompt(number, msg) {
   return `Player ${number}: ${msg}`;
 }
 
@@ -67,24 +67,25 @@ let computerPlay = () => {
   console.log(`*** Round ${totalRound} ***`);
   console.log(`Make your choice: ${CHOICES_DEFINITION.join(', ')}?\n`);
 
-  playerOneChoice = USER_INPUT.question(prompt('', 'ONE')).toLowerCase();
+  playerOneChoice = USER_INPUT.question(prompt('ONE', '')).toLowerCase();
   playerOneChoice = shortenChoices(playerOneChoice);
   playerOneChoice = playerChoiceValidation(playerOneChoice);
 
   playerTwoChoice = computerTurn();
   console.clear();
 };
+
 //if play with another person
 let friendPlay = () => {
   console.log(`*** Round ${totalRound} ***`);
   console.log(`Make your choice: ${CHOICES_DEFINITION.join(', ')}?\n`);
 
-  playerOneChoice = USER_INPUT.question(prompt('', 'ONE')).toLowerCase();
+  playerOneChoice = USER_INPUT.question(prompt('ONE', '')).toLowerCase();
   playerOneChoice = shortenChoices(playerOneChoice);
-  playerChoiceValidation(playerOneChoice);
+  playerOneChoice = playerChoiceValidation(playerOneChoice);
   console.clear();
 
-  playerTwoChoice = USER_INPUT.question(prompt('', 'TWO')).toLowerCase();
+  playerTwoChoice = USER_INPUT.question(prompt('TWO', '')).toLowerCase();
   playerTwoChoice = shortenChoices(playerTwoChoice);
   playerTwoChoice = playerChoiceValidation(playerTwoChoice);
   console.clear();
@@ -118,15 +119,16 @@ console.clear();
 console.log(`Welcome to the ${CHOICES.join(', ')} game.\n`);
 
 let playAgain = 'yes';
-//let nextRound = USER_INPUT.question("Press 'enter' to start next round...");
 
 while (playAgain === 'yes' || playAgain === 'y') {
   totalRound = 1;
   playerOneCounter = 0;
   playerTwoCounter = 0;
+
   let whoIsPlayerTwo = USER_INPUT.question("Would you like to play against me or a friend? 1)ME 2)FRIEND: ");
   playerValidation(whoIsPlayerTwo);
   console.clear();
+
   while ((playerOneCounter < 5) || (playerTwoCounter < 5)) {
     console.clear();
     if (whoIsPlayerTwo === '1') {
@@ -137,7 +139,7 @@ while (playAgain === 'yes' || playAgain === 'y') {
       gameLogic();
     } else {
       if (totalRound === 1) {
-        console.log("Alright, First one to 5 wins! let starts!\n");
+        console.log("Alright, First one to 5 wins! let starts!\n\n");
       }
       friendPlay();
       gameLogic();
@@ -155,12 +157,14 @@ while (playAgain === 'yes' || playAgain === 'y') {
 
     console.log(`Score: Player ONE: ${playerOneCounter}, Player TWO: ${playerTwoCounter}\n`);
     totalRound += 1;
-    //Stop te game until user press enter
+
+    //Stop the game until user press enter
     let nextRound = USER_INPUT.question("Press 'enter' to start next round...");
     if (nextRound) {
       continue;
     }
   }
+
   playAgain = isPlayAgainValid(USER_INPUT.question('\nWould you like to play again? 1)Yes 2)No: ').toLowerCase());
   console.clear();
 }
